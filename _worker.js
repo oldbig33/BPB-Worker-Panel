79d3d8c5611b2f584d69de4767f062522afbe437
oldbig33 <106166383+oldbig33@users.noreply.github.com>
2024-07-24 07:04:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -200,8 +200,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        /* url.hostname = 'www.speedtest.net';
-*/			url.hostname = 'www.2345.com';
+			url.hostname = 'www.2345.com';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
